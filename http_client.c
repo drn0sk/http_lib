87995d695e5780bd730ee2aac85d24181cbdff93
drn0sk <DarshanS2912@gmail.com>
2026-08-11 19:42:16 -0400

    HTTP is used in the request line for both http and https

--- http_client.c
+++ http_client.c
@@ -256,7 +256,7 @@ static bool _http_request(Method m, char *hostname, char *location, char *protoc
 	*hdrs_end = '\0';
 	*h = NULL;
 	char *request;
-	if(asprintf(&request, "%s %s%s%s%s%s %s/1.1\r\n%s\r\n", strmeth(m), location, (query)?"?":"", (query)?query:"", (frag)?"#":"", (frag)?frag:"", (https) ? "HTTPS" : "HTTP", hdrs) == -1) {
+	if(asprintf(&request, "%s %s%s%s%s%s HTTP/1.1\r\n%s\r\n", strmeth(m), location, (query)?"?":"", (query)?query:"", (frag)?"#":"", (frag)?frag:"",  hdrs) == -1) {
 		if(logfile >= 0) dprintf(logfile, "Error: failed to create request string.\n");
 		socket_close(conn, logfile);
 		if(conn.type == SSL_CONN) SSL_CTX_free(conn.ctx);
